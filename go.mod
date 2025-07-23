92bdf37d975b757654e8ea5edad4916becf19346
Meta <meta_apex@gmail.com>
2025-07-23 10:58:08 +0800

    add

--- go.mod
+++ go.mod
@@ -6,7 +6,15 @@ require (
 	github.com/BurntSushi/toml v1.5.0
 	github.com/fsnotify/fsnotify v1.9.0
 	github.com/joho/godotenv v1.5.1
+	github.com/panjf2000/ants/v2 v2.11.3
+	github.com/spaolacci/murmur3 v1.1.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
-require golang.org/x/sys v0.13.0 // indirect
+require (
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	golang.org/x/sync v0.11.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+)
---
+++ rescue/recover.go
@@ -0,0 +1,32 @@
+package rescue
+
+import (
+	"context"
+	"fmt"
+	"github.com/meta-apex/gopkg/zlog"
+)
+
+// Recover is used with defer to do cleanup on panics.
+// Use it like:
+//
+//	defer Recover(func() {})
+func Recover(cleanups ...func()) {
+	for _, cleanup := range cleanups {
+		cleanup()
+	}
+
+	if p := recover(); p != nil {
+		zlog.Error().Stack().Msg(fmt.Sprint(p))
+	}
+}
+
+// RecoverCtx is used with defer to do cleanup on panics.
+func RecoverCtx(ctx context.Context, cleanups ...func()) {
+	for _, cleanup := range cleanups {
+		cleanup()
+	}
+
+	if p := recover(); p != nil {
+		zlog.Error().Stack().Msg(fmt.Sprint(p))
+	}
+}
